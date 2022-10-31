9ecf43e33943e23a7a6c9da08c4bcf52cd2ebe5e
Samantha González <samujm76@gmail.com>
2022-10-30 20:13:23 -0600

    Servir un sitio web completo

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ app.use(express.static('public'));
 //     res.send("Home page");
 // });
 
-app.get("/hola-mundo", (req, res)=> {
-    res.send("Hola mundo en su respectiva ruta");
+app.get("/generic", (req, res)=> {
+    res.sendFile( __dirname + '/public/generic.html');
 });
+
+app.get("/elements", (req, res)=> {
+    res.sendFile( __dirname + '/public/elements.html');
+});
+
 
 app.get("*", (req, res)=> {
     res.sendFile( __dirname + '/public/404.html');
