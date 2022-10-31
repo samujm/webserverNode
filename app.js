b642483092074b5510c3cb40884cfe2ad9618a71
Samantha González <samujm76@gmail.com>
2022-10-30 19:06:44 -0600

    Servir contenido estático, varias páginas establecidas

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res)=> {
-    res.send("Home page");
-});
+// Servir contenido estático
+app.use(express.static('public'));
+//Al entrar al path del / (anterior linea) lo siguiente ya no se ejecuta
+
+//Esto ya no se ejecuta
+// app.get("/", (req, res)=> {
+//     res.send("Home page");
+// });
 
 app.get("/hola-mundo", (req, res)=> {
     res.send("Hola mundo en su respectiva ruta");
 });
 
 app.get("*", (req, res)=> {
-    res.send("404 | Page not found");
+    res.sendFile( __dirname + '/public/404.html');
 });
 
 app.listen(port, () => {
